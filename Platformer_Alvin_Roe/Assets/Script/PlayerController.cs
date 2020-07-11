49fed9b6808b434d1e2dc7d9fdec1d90a4ddd727
Marthon <marcibaumel@gmail.com>
2020-07-11 17:38:28 +0200

    -12.videó

--- Platformer_Alvin_Roe/Assets/Script/PlayerController.cs
+++ Platformer_Alvin_Roe/Assets/Script/PlayerController.cs
@@ -16,20 +16,28 @@ public class PlayerController : MonoBehaviour
     
     private void Update()
     {
+        
+
         //Moving with A and D in the horizontal area
         if(Input.GetKey(KeyCode.A  )){
             rb.velocity=new Vector2(-5,rb.velocity.y);
+            transform.localScale= new Vector2(-1, 1);
         }else if(Input.GetKey(KeyCode.LeftArrow )){
             rb.velocity=new Vector2(-5,rb.velocity.y);
+            transform.localScale= new Vector2(-1, 1);
         }else if(Input.GetKey(KeyCode.D)){
             rb.velocity=new Vector2(5,rb.velocity.y);
+            transform.localScale= new Vector2(1, 1);
         }else if(Input.GetKey(KeyCode.RightArrow)){
             rb.velocity=new Vector2(5,rb.velocity.y);
+            transform.localScale= new Vector2(1, 1);
         }
         
         //Jump
         if (Input.GetKeyDown(KeyCode.Space)){
-            rb.velocity=new Vector2(rb.velocity.x, 10f);
-        }
+             rb.velocity=new Vector2(rb.velocity.x, 18f);
+        }else if(Input.GetKeyDown(KeyCode.UpArrow)){
+           rb.velocity=new Vector2(rb.velocity.x, 18f); 
+        } 
     }
 }
