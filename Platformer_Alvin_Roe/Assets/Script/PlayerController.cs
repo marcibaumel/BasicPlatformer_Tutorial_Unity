b964a6320bf0c9aed43ea1581f0c38780dccdc43
Marthon <marcibaumel@gmail.com>
2020-07-11 15:10:06 +0200

    -8.videó+arrow gombok

---
+++ Platformer_Alvin_Roe/Assets/Script/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        
+        
+    }
+
+    
+    private void Update()
+    {
+        //Moving with A and D in the horizontal area
+        if(Input.GetKey(KeyCode.A  )){
+            rb.velocity=new Vector2(-5,rb.velocity.y);
+        }else if(Input.GetKey(KeyCode.LeftArrow )){
+            rb.velocity=new Vector2(-5,rb.velocity.y);
+        }else if(Input.GetKey(KeyCode.D)){
+            rb.velocity=new Vector2(5,rb.velocity.y);
+        }else if(Input.GetKey(KeyCode.RightArrow)){
+            rb.velocity=new Vector2(5,rb.velocity.y);
+        }
+        
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity=new Vector2(rb.velocity.x, 10f);
+        }
+    }
+}
