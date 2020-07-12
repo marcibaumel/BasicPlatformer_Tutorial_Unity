336c6d6c52ac67d2e59f3a7d8ee8582583a443e1
Marthon <marcibaumel@gmail.com>
2020-07-12 14:20:16 +0200

    -16.videó

--- Platformer_Alvin_Roe/Assets/Script/PlayerController.cs
+++ Platformer_Alvin_Roe/Assets/Script/PlayerController.cs
@@ -5,40 +5,80 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
     
+    private enum State {idle, running, jump, falling};
+    private State state=State.idle;
+    private Collider2D coll;
+    [SerializeField]private LayerMask ground;
 
-    void Start()
+
+    private void Start()
     {
-        
-        
+        rb=GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
+        coll=GetComponent<Collider2D>();
     }
 
     
     private void Update()
     {
         
-        
-        //Moving with A and D in the horizontal area
-        if(Input.GetKey(KeyCode.A  )){
+        float hdirection=Input.GetAxis("Horizontal");
+        float vdirection=Input.GetAxis("Vertical");        
+        if(hdirection<0){
             rb.velocity=new Vector2(-5,rb.velocity.y);
             transform.localScale= new Vector2(-1, 1);
-        }else if(Input.GetKey(KeyCode.LeftArrow )){
-            rb.velocity=new Vector2(-5,rb.velocity.y);
-            transform.localScale= new Vector2(-1, 1);
-        }else if(Input.GetKey(KeyCode.D)){
+            
+        }else if(hdirection>0){
             rb.velocity=new Vector2(5,rb.velocity.y);
             transform.localScale= new Vector2(1, 1);
-        }else if(Input.GetKey(KeyCode.RightArrow)){
-            rb.velocity=new Vector2(5,rb.velocity.y);
-            transform.localScale= new Vector2(1, 1);
+            
         }
+       else
+       {
+           
+       }
         
         //Jump
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground)){
              rb.velocity=new Vector2(rb.velocity.x, 18f);
-        }else if(Input.GetKeyDown(KeyCode.UpArrow)){
-           rb.velocity=new Vector2(rb.velocity.x, 18f); 
-        } 
+             state=State.jump;
+        }
+
+        VelocityStateSwitch();
+        anim.SetInteger("state", (int)state);
     }
+
+    private void VelocityStateSwitch()
+    {
+        if(state == State.jump)
+        {
+            if(rb.velocity.y<0.1f)
+            {
+                state=State.falling;
+            }
+
+        }
+        else if(state==State.falling)
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                state=State.idle;
+            }
+        }
+        else if(Mathf.Abs(rb.velocity.x)>2f)
+        {
+            //Going right
+            state =State.running;
+        }
+        else{
+            state=State.idle;
+        }
+        
+
+    }
+
+
 }
