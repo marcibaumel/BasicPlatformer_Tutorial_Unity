0c39dc8dc3a74ac7a2ce030937e43db146c7d825
Marthon <marcibaumel@gmail.com>
2020-07-13 19:29:23 +0200

    -jump+enemy

--- Platformer_Alvin_Roe/Assets/Script/PlayerController.cs
+++ Platformer_Alvin_Roe/Assets/Script/PlayerController.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour
 {
     //Start() Variables
     private Rigidbody2D rb;
     private Animator anim;
-    public int cherries=0;
+    
     
     //FSM
-    private enum State {idle, running, jump, falling};
+    private enum State {idle, running, jump, falling, hurt};
     private State state=State.idle;
     private Collider2D coll;
 
@@ -18,7 +20,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField]private LayerMask ground;
     [SerializeField] private float speed=7f;
     [SerializeField] private float jumpForce=10f;
-    //[SerializeField] private int cherries=0;
+    [SerializeField] private int cherries=0;
+    [SerializeField] private Text cherryText; 
+    [SerializeField] private float hurtForce=10f;
 
 
     private void Start()
@@ -32,9 +36,14 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
 
-        Movement();
+        if(state != State.hurt)
+        {
+            Movement();
+        }
+        //Movement();
         AnimationState();
         anim.SetInteger("state", (int)state);
+
     }
 
      private void OnTriggerEnter2D(Collider2D collision) //Trigger for Collectables
@@ -43,12 +52,41 @@ public class PlayerController : MonoBehaviour
         {
             Destroy(collision.gameObject); //Cherry destroy
             cherries += 1;
+            cherryText.text = cherries.ToString();
+
         }
+    }
+
+     private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.tag =="Enemy")
+        {
+            if(state==State.falling)
+            {
+                Destroy(other.gameObject);
+                Jump();
+            }
+            else
+            {
+                state=State.hurt;
+                if(other.gameObject.transform.position.x>transform.position.x)
+                {
+                    rb.velocity=new Vector2(-hurtForce, rb.velocity.y);
+                }
+                else
+                {
+                    rb.velocity=new Vector2(hurtForce, rb.velocity.y);
+                }
+            }
+            
+        }
+    
     }
 
     private void Movement(){
         float hdirection=Input.GetAxis("Horizontal");
-        float vdirection=Input.GetAxis("Vertical");        
+        float vdirection=Input.GetAxis("Vertical");    
+
         if(hdirection<0){
             rb.velocity=new Vector2(-speed,rb.velocity.y);
             transform.localScale= new Vector2(-1, 1);
@@ -60,13 +98,18 @@ public class PlayerController : MonoBehaviour
        
         //Jump
         if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground)){
-             rb.velocity=new Vector2(rb.velocity.x, jumpForce);
-             state=State.jump;
+             Jump();
         }
+    }
+    private void Jump()
+    {
+        rb.velocity=new Vector2(rb.velocity.x, jumpForce);
+        state=State.jump;
     }
 
     private void AnimationState()
     {
+
         if(state == State.jump)
         {
             if(rb.velocity.y<0.1f)
@@ -78,6 +121,13 @@ public class PlayerController : MonoBehaviour
         else if(state==State.falling)
         {
             if(coll.IsTouchingLayers(ground))
+            {
+                state=State.idle;
+            }
+        }
+        else if(state==State.hurt)
+        {
+            if(Mathf.Abs(rb.velocity.x)<0.1f)
             {
                 state=State.idle;
             }
