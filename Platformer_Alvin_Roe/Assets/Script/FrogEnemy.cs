f839f311b51c528f940414f3405801aaa98d42f9
Marthon <marcibaumel@gmail.com>
2020-07-14 11:37:19 +0200

    -ai

---
+++ Platformer_Alvin_Roe/Assets/Script/FrogEnemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogEnemy : MonoBehaviour
+{
+    [SerializeField]private float leftCap;
+    [SerializeField]private float rightCap;
+
+    [SerializeField] private float jumpLength;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private LayerMask ground;
+    private Collider2D coll;
+    private Rigidbody2D rb;
+    private bool facingLeft=true;
+
+    void Start()
+    {
+        coll=GetComponent<Collider2D>();
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if(facingLeft)
+        {
+            
+            if(transform.position.x > leftCap)
+            {
+                if(transform.localScale.x != 1)
+                {
+                    transform.localScale=new Vector3(1,1);
+                }
+                if(coll.IsTouchingLayers(ground))
+                {
+                    //Jump
+                    rb.velocity=new Vector2(-jumpLength, jumpHeight);
+                }
+               
+            }
+            
+            
+            else
+            {
+                facingLeft=false;
+            }
+        }
+        else
+        {
+           if(transform.position.x<rightCap)
+            {
+                if(transform.localScale.x!=-1)
+                {
+                    transform.localScale=new Vector3(-1,1);
+                }
+                if(coll.IsTouchingLayers(ground))
+                {
+                    //Jump
+                    rb.velocity=new Vector2(jumpLength, jumpHeight);
+                }
+            }
+            else
+            {
+                facingLeft=true;
+            } 
+
+        }
+    }
+    
+}
